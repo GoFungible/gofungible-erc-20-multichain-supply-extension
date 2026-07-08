69238465e36085881251cbf697603887d51469df
pellyadolfo <pellyadolfo@yahoo.es>
2026-07-08 20:20:48 +0200

    updated interface

--- contracts/IERC20x.sol
+++ contracts/IERC20x.sol
@@ -5,7 +5,26 @@ pragma solidity 0.8.30;
  * @dev Interface of the ERC-20 standard as defined in the ERC.
  */
 interface IERC20x {
+
+
+
+
+		event MasterChainUpdated(uint256 fromMasterChain, uint256 toMasterChain);
+
+		function getMasterChain() external view returns (uint256);
+
+		function setMasterChain(uint256 masterChain_) external;
+
+
+
 	
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function globalSupply() external view returns (uint256);
+
+
+
     /**
      * @dev Emitted when `value` tokens are moved from one account (`from`) to
      * another (`to`).
@@ -13,16 +32,6 @@ interface IERC20x {
      * Note that `value` may be zero.
      */
     event TransferX(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Returns the value of tokens in existence.
-     */
-    function globalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the value of tokens owned by `account`.
-     */
-    //function balanceOfX(address account) external view returns (uint256);
 
     /**
      * @dev Moves a `value` amount of tokens from the caller's account to `to`.
@@ -33,15 +42,5 @@ interface IERC20x {
      */
     function transferX(uint256 toChain, address toAddress, uint256 amount) external returns (bool);
 
-    /**
-     * @dev Moves a `value` amount of tokens from `from` to `to` using the
-     * allowance mechanism. `value` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    //function transferXFrom(address from, uint256 toChain, address toAddress, uint256 amount) external returns (bool);
 
 }
